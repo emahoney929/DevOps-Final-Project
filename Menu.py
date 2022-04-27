abb6213546b6b9a00e46c0cacfaa201b8c93ac7d
Ethan Mahoney <60744824+emahoney929@users.noreply.github.com>
2022-04-26 22:59:44 -0700

    comments added

--- Menu.py
+++ Menu.py
@@ -1,3 +1,7 @@
+# Author: Ethan Mahoney
+# Class: CS491 - DevOps
+# Poject: Menu class used in conjuction with driver file to
+# control flow of the cmd line menu provided in the program
 from Player import Player
 from Scraper import Scraper
 
@@ -8,16 +12,10 @@ class Menu:
         self.players = []
         self.playerCount = 0
 
-
-    def setChoice(self, choice):
-        self.choice = choice
-
+    # Getters
     def getChoice(self):
         return self.choice
-
-    def increaseCount(self):
-        self.playerCount += 1
-
+    
     def getPlayerCount(self):
         return self.playerCount
 
@@ -29,16 +27,27 @@ class Menu:
             if player.getFirstName() == first and player.getLastName() == last:
                 return player
         return None
+    
+    # Setter
+    def setChoice(self, choice):
+        self.choice = choice
 
+    # tracks the amount of unique players searched
+    def increaseCount(self):
+        self.playerCount += 1
+
+    # checks for player in searched list
     def hasPlayer(self, first, last):
         for player in self.players:
             if player.getFirstName() == first and player.getLastName() == last:
                 return True
         return False
 
+    # adds a player to the list of people search
     def addPlayer(self, player):
         self.players.append(player)
 
+    # returns the stats of a player given a defined year
     def getPlayerStats(self, player, year):
         return self.scraper.scrapeInfo(player, year)
 
