995bc1060f7472c0337bcd8cba1db88008ef40db
Ethan Mahoney <60744824+emahoney929@users.noreply.github.com>
2022-04-26 23:03:36 -0700

    comments added

--- Player.py
+++ Player.py
@@ -1,3 +1,6 @@
+# Author: Ethan Mahoney
+# Class: CS491 - DevOps
+# Project: Player class used to hold information regarding a baseball player
 class Player:
     def __init__(self, first="", last=""):
         self.firstName = first
@@ -6,22 +9,25 @@ class Player:
                              "3B", "HR", "RBI", "SB", "CS", "BB", "SO", "BA", "OBP", "SLG", 
                              "OPS", "OPS+", "TB", "GDP", "HBP",	"SH", "SF",	"IBB", "Pos", "Awards"]]
 
+    # Setter
     def setFullName(self, first, last):
         self.firstName = first.lower()
         self.lastName = last.lower()
 
+    # Getters
     def getFirstName(self):
         return self.firstName 
         
     def getLastName(self):
         return self.lastName
+    
+    def getStats(self):
+        return self.playerStats
 
     def addStats(self, stats):
         self.playerStats.append(stats)
 
-    def getStats(self):
-        return self.playerStats
-
+    # formats the string output of a player object for printing
     def __str__(self):
         output = ""
         for col in self.playerStats[0]:
