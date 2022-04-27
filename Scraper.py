8e786964a0d1bb38d073b6f86b61e1a7eaa5fcd2
Ethan Mahoney <60744824+emahoney929@users.noreply.github.com>
2022-04-26 23:07:15 -0700

    comments added

--- Scraper.py
+++ Scraper.py
@@ -1,3 +1,7 @@
+# Author: Ethan Mahoney
+# Class: CS491 - DevOps
+# Project: Scraper class used to webscrape player information from 
+# Baseball Reference website using Beautiful Soup
 from bs4 import BeautifulSoup
 from requests import get
 
@@ -6,14 +10,17 @@ class Scraper:
         self.link = ""
         self.html = None
 
+    # Constructs a link based off the name of the baseball player provided
     def constructLink(self, first, last):
         if len(last) > 5:
             last = last[:5]
         first = first[0:2]
 
+        # Link is similar for all players except for sublte differnces depending on last and first name
         return "https://www.baseball-reference.com/players/" + last[0] + "/" + last + first + "01.shtml"
 
 
+    # Uses beautiful soup to scrape information from the web link and add the stats to a list
     def scrapeInfo(self, player, year):
         self.link = self.constructLink(player.getFirstName(), player.getLastName())
         html = get(self.link).text
@@ -30,5 +37,5 @@ class Scraper:
                     stats.append(datum.text)
                     
         player.playerStats.append(stats)
-        return player
+        return player   # Returns a player object that will be added to the list of searched players
         
