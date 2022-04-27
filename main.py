33ce5e98d74153140b1af3fa4bad6e153f74960b
Ethan Mahoney <60744824+emahoney929@users.noreply.github.com>
2022-04-26 23:10:46 -0700

    comments added

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Author: Ethan Mahoney
+# Class: CS491 - DevOps
+# Project: Driver file for the DevOps final project
+# Runs a program that scrapes baseball player stats
+# based off user input
 from Menu import Menu
 from Player import Player
 import scraperTests
@@ -7,9 +12,9 @@ if __name__ == "__main__":
     print("---------------------------------------------------")
     print("This program provides player statistics based on the name of the player entered!\n")
 
-
     menu = Menu()
 
+    # Loops menu until option 3 is chosen
     while True:
         print("Please select an option:")
         print("1. Retrieve Player Stats")
@@ -25,11 +30,13 @@ if __name__ == "__main__":
             try:
                 player = menu.getPlayerStats(Player(first, last), year)
 
+                # If a player has already been searched, append new stats to the list of the specified player stats
                 if menu.hasPlayer(player.getFirstName(), player.getLastName()):
                     play = menu.getPlayer(player.getFirstName(), player.getLastName())
                     play.addStats(player.getStats()[1])
                     print(play)
                 else:
+                    # If player has not been searched, add them to list of searched players
                     menu.addPlayer(player)
                     menu.increaseCount()
                     print(menu.players[menu.getPlayerCount()-1])
@@ -37,6 +44,8 @@ if __name__ == "__main__":
                 print("\nError: Could not get player stats! (Could be misspelled name or invalid year)\n")
         elif menu.getChoice() == "2":
             first, last = input("\nWhich player would you like to save stats for (first last): ").split()
+            
+            # Prints the player stats to a file named after baseball player
             if menu.hasPlayer(first, last):
                 try:
                     with open(first + last + ".txt", "w") as outfile: 
@@ -48,7 +57,7 @@ if __name__ == "__main__":
             else:
                 print("\nError: Must retrieve a player's stats or spelt player name incorrectly!\n")
         elif menu.getChoice() == "3":
-            break
+            break   # End program
         else:
             print("\nError: Please enter a valid option!")
 
